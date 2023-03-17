40af01593b607d48a3dedc65df34b1d4359f820f
yosprasong3456 <yosprasong3456@gmail.com>
2023-03-17 10:28:13 +0700

    edit db

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,13 +1,15 @@
+require('dotenv').config();
+
 const knex = require("knex")
 module.exports ={
   knexBuilder : knex({
     client: "mysql",
     connection: {
-      host: "192.168.254.170",
-      port: 3306,
-      user: "Apisit",
-      password: "Apisit@udch",
-      database: "rad",
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
     },
   })
 }
